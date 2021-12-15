53fec19e1d81c59fb5b476b83e064ba98408bfc2
RScodeSec <70410425+RScodeSec@users.noreply.github.com>
2021-12-15 14:30:42 -0500

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app.config['SESSION_TYPE'] = 'filesystem'
 
 Session(app)
 
-socketio = SocketIO(app, manage_session=False)
+socketio = SocketIO(app, cors_allowed_origins=['https://inspirados2021.herokuapp.com'],manage_session=False)
 
 
 @app.route('/', methods=['GET', 'POST'])
