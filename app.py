fa096a0c5e78e1fbd6b4646fa240da8de8a6409a
RScodeSec <70410425+RScodeSec@users.noreply.github.com>
2021-12-15 13:30:52 -0500

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ app.config['SESSION_TYPE'] = 'filesystem'
 Session(app)
 
 socketio = SocketIO(app, manage_session=False)
+socketio.init_app(app, cors_allowed_origins="*")
 
 
 @app.route('/', methods=['GET', 'POST'])
